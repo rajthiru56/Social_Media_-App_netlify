db5a1f499762e9cec075b037ce03240c92972a90
rajthiru56 <rajthiru56@gmail.com>
2024-05-15 14:33:01 +0530

    first commit

---
+++ src/About.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <main className='About'>
+        <h2>About</h2>
+        <p style={{ marginTop:"1rem" }}>Hi, I am Raja Together lets make education free for everyone in the world </p>
+    </main>
+
+  )
+}
+
+export default About
---
+++ src/Post.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import { Link } from "react-router-dom"
+
+const Post = ({post}) => {
+  return (
+    <article className='post'>
+      <Link to={`post/${post.id}`}> 
+        <h2>{post.title}</h2>
+        <p className='postdate'>{post.datetime}</p>
+      </Link>
+      
+      <p className='postBody'>{(post.body).length <=25 ? post.body : `${(post.body).slice(0,25)}...`}</p>
+      <hr></hr>
+      
+    </article>
+  )
+}
+
+export default Post
+
+
+
